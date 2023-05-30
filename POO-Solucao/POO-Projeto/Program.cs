e67e71ebd49ed4b7e4c82b4c0f2b6bc5ff081f72
Carlos Roberto Stunitz <carlos.stunitz@outlook.com>
2023-05-30 12:48:57 -0300

    Versao Inicial da Aplicação POO

--- POO-Solucao/POO-Projeto/Program.cs
+++ POO-Solucao/POO-Projeto/Program.cs
@@ -1,2 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+using Aula02;
+using System.Drawing;
+
+
+void Imprimir(TFigura figura)
+{
+    figura.Imprimir();
+    figura.Desenhar();
+    figura.Exibir();
+}
+
+TFigura figura1 = new TQuadrado(300, Color.BlueViolet);
+TFigura figura2 = new TCirculo(150, Color.GreenYellow);
+
+Console.WriteLine("### Dados da Figura 1 ###");
+Imprimir(figura1);
+
+Console.WriteLine(" ");
+Console.WriteLine("### Dados da Figura 2 ###");
+Imprimir(figura2);
+
+Console.Write("pressione uma tecla para finalizar...");
+Console.ReadKey();
+
+
+//((TQuadrado)figura1).Lado = 4;
+//((TQuadrado)figura1).Cor = Color.Red;
+
+
+/*
+TFigura figura1 = new TQuadrado();
+TFigura figura2 = new TCirculo();
+
+((TQuadrado)figura1).Lado = 4;
+((TCirculo)figura2).Raio = 4;
+
+figura1.CalcularArea();
+figura2.CalcularArea();
+
+Console.WriteLine("### Areas calculadas ###");
+Console.WriteLine("Figura1: " + figura1.Area);
+Console.WriteLine("Figura2: " + figura2.Area);
+Console.WriteLine(" ");
+Console.Write("pressione uma tecla para finalizar...");
+Console.ReadLine();
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+TQuadrado figura = new TQuadrado();
+
+figura.Cor = Color.Red;
+figura.Lado = 8;
+figura.CalcularArea();
+
+Console.WriteLine("### Area do quadrado ###");
+Console.WriteLine("==> Cor informada: " + figura.Cor.ToString());
+Console.WriteLine("==> Lado.........: " + figura.Lado);
+Console.WriteLine("==> Area.........: " + figura.Area);
+Console.WriteLine(" ");
+Console.Write("pressione uma tecla para finalizar...");
+Console.ReadLine();
+*/
---
+++ POO-Solucao/POO-Projeto/Quadrado.cs
@@ -0,0 +1,61 @@
+﻿
+using FigureControlLibrary;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Aula02
+{
+    public class TQuadrado : TFigura
+    {
+        private double lado;
+
+        public double Lado { get => lado; set => lado = value; }
+
+        // construtores
+        public TQuadrado() 
+        { 
+        }
+
+        public TQuadrado(double lado)
+        {
+            this.lado = lado;
+            CalcularArea();
+        }
+
+        public TQuadrado(double lado, Color cor)
+        {
+            this.lado = lado;
+            Cor = cor;
+            CalcularArea();
+        }
+
+        public override void CalcularArea()
+        {
+            Area = lado * lado;
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine("Tipo: " + GetType().Name);
+            Console.WriteLine("Cor.: " + Cor.ToString());
+            Console.WriteLine("Lado: " + lado);
+            Console.WriteLine("Area: " + Area);
+        }
+
+        private const string arquivo = "FiguraQuadrado.png";
+
+        public override void Desenhar()
+        {
+            TDesenhista artista = new TDesenhista();
+
+            artista.DesenharQuadrado(Cor, (int)lado, arquivo);
+
+            Console.WriteLine(GetType().Name + " salvo no arquivo " + arquivo);
+        }
+
+        public override void Exibir()
+        {
+            Process.Start(Aplicativo, arquivo);
+        }
+    }
+}
